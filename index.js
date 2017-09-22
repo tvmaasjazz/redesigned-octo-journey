17afc89c238fc02a2374cc9eda178b2205a41ba5
Kan Adachi <kadachi1023@gmail.com>
2017-09-22 14:49:55 -0700

    hello controller

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+const helloController = require('./helloController');
+
+app.get('/hello', helloController);
 
 app.get('/', (req, res) => res.json({ hello: 'world' }));
 
